8639c1f02a4c7b9b0a3fccaa248f8edfe192f76f
Mina Kim <rlaqls16@gmail.com>
2021-03-09 19:55:12 +0900

    FrameDemo
    프레임 예제 300, 300 사이즈 만들기

---
+++ FremeDemo/Demo.java
@@ -0,0 +1,13 @@
+package FremeDemo;
+import java.awt.Frame;
+
+public class Demo {
+	public static void main(String[] args)
+	{
+		Frame 프레임1 = new Frame();
+		
+		프레임1.setTitle("프레임 예제");
+		프레임1.setSize(300, 300);
+		프레임1.setVisible(true);
+	}
+}
